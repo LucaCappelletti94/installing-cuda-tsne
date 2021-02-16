4edff00e9b46737ccf857c4a1b5d9dd019be844a
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2021-02-16 22:12:09 +0100

    Updated dockerfile

--- install_cuda_tsne.sh
+++ install_cuda_tsne.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+apt remove cmake
+pip install cmake --upgrade
+bash
 git clone https://github.com/rmrao/tsne-cuda.git && cd tsne-cuda
 git submodule init
 git submodule update
 cd build/
-#cmake ..
+cmake ..
